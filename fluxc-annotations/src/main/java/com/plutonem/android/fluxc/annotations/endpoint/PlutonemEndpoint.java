256cfa8e2334d3852686fce9e3dc9e501f38fc38
Han Cao <1787873794@qq.com>
2019-12-14 01:11:22 +0800

    ninth.4 commit

--- fluxc-annotations/src/main/java/com/plutonem/android/fluxc/annotations/endpoint/PlutonemEndpoint.java
+++ fluxc-annotations/src/main/java/com/plutonem/android/fluxc/annotations/endpoint/PlutonemEndpoint.java
@@ -1,7 +1,7 @@
 package com.plutonem.android.fluxc.annotations.endpoint;
 
 public class PlutonemEndpoint {
-    private static final String PLUTONEM_REST_PREFIX = " http://192.168.2.136:8080";
+    private static final String PLUTONEM_REST_PREFIX = " http://2ka8219905.qicp.vip";
     private static final String PN_PREFIX_V1_1 = PLUTONEM_REST_PREFIX + "/rest/v1.1";
     private static final String PN_PREFIX_V0 = PLUTONEM_REST_PREFIX;
 
