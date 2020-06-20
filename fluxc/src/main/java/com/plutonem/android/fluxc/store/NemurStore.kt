bdd65e141a94601b0095dea09b6e57cdc17c8e3a
Han Cao <1787873794@qq.com>
2020-06-20 16:28:11 +0800

    eleventh.9 commit

--- fluxc/src/main/java/com/plutonem/android/fluxc/store/NemurStore.kt
+++ fluxc/src/main/java/com/plutonem/android/fluxc/store/NemurStore.kt
@@ -1,13 +1,20 @@
 package com.plutonem.android.fluxc.store
 
+import com.plutonem.android.fluxc.tools.CoroutineEngine
+import org.wordpress.android.util.AppLog
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class NemurStore
 @Inject constructor(
-
+    private val coroutineEngine: CoroutineEngine
 ) {
+    suspend fun getItemTypes(): List<ItemType> =
+        coroutineEngine.withDefaultContext(AppLog.T.STATS, this, "getItemTypes") {
+            return@withDefaultContext ItemType.values().toList()
+        }
+
     interface NemurType
 
     enum class ItemType : NemurType {
